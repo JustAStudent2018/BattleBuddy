3efcc3c2cd4c77fb7954ee662c87cac1798df2ab
JustAStudent2018 <medardo.sosobrado@elev.cybergymnasiet.se>
2021-04-13 00:00:25 +0200

    Update

--- Website2.0/game.js
+++
@@ -1,6 +0,0 @@
-function mobile320() {
-    document.querySelector(".browser1").setAttribute("src", "https://tfue.io/3d");
-  }
-  
-  document.getElementsByTagName("button")[0].onclick = mobile320;
-  
